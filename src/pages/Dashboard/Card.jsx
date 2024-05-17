535770ec5ae0c263731735a67c30aaa0c600f1c1
ajayraj2001 <ajayraj072001@gmail.com>
2024-05-18 01:09:16 +0530

    added category

--- src/pages/Dashboard/Card.jsx
+++ src/pages/Dashboard/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Card = ({item}) => {
   return (
     <Link to={`/users/${item.categoryName}/${item._id}`}>
-<div className={`w-[320px] px-4 my-4 mx-4 py-6 border-1 text-white shadow ${item.bcolor} rounded-lg cursor-pointer`}>
+<div className={`w-[320px] px-4 my-4 mx-4 py-6 border-1 text-white shadow ${item.bcolor} rounded-lg ${item.name === "Total Clients" && 'cursor-pointer'}`}>
    <div className='flex justify-between items-center'> 
    <a href="#" className='mb-0'>
         <h5 className="mb-0 text-2xl font-bold">{item.name}</h5>
